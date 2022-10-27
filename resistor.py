2ade19687226229ddf219bb34b5ce35c678cc6c9
Simone Cominato <simone.cominato@gmail.com>
2022-10-27 07:54:45 +0200

    First commit

---
+++ resistor.py
@@ -0,0 +1,135 @@
+import numpy as np
+
+class Resistor():
+
+    @property
+    def r_value(self):
+        return self._r_value
+    
+    @r_value.setter
+    def r_value(self, value):
+        if value < 0:
+            raise ValueError("r_value must be >= 0")
+        
+        self._r_value = value
+    
+    @property
+    def r_tolerance(self):
+        return self._r_tolerance
+    
+    @r_tolerance.setter
+    def r_tolerance(self, value):
+        tolerance_list = np.array([1, 2, 5, 10])
+        if not np.any(tolerance_list == value):
+            raise ValueError(f"value must be one of {tolerance_list}")
+        else:
+            self._r_tolerance = value
+    
+    @property
+    def r_power(self):
+        return self._r_power
+    
+    @r_power.setter
+    def r_power(self, value):
+        if value < 0:
+            raise ValueError("r_power must be >= 0")
+        
+        self._r_power = value
+    
+    __r_std = 0
+    
+    def __init__(self,
+                 *,
+                 value: float = 0,
+                 tolerance: int = 5,
+                 power: float = 0.25) -> None:
+        
+        self.r_value = value
+        self.r_tolerance = tolerance
+        self.r_power = power
+    
+    def __str__(self) -> str:
+        
+        res_description = f"r_value: {self.r_value}\n"\
+                          f"r_tolerance: {self.r_tolerance}\n"\
+                          f"r_power: {self.r_power}\n"
+        
+        return res_description
+    
+    @classmethod
+    def __E_series(cls,
+                   r_value: float,
+                   r_tolerance: float) -> float:
+        
+        e_series = {1: 96, 2: 48, 5: 24, 10: 12}
+        m = e_series[r_tolerance]
+
+        decimals = 0
+        if r_tolerance >= 5:
+            decimals = 1
+        else:
+            decimals = 2
+
+        exp = 0
+        res = r_value
+        while (res >= 10.0):
+            res /= 10
+            exp += 1
+        
+        r_low = 1 * (10 ** decimals)
+        r_value_adj = r_value * (10 ** (decimals - exp))
+        for n in range(1, m):
+            r_high = int(round(10 ** (decimals + (float(n) / float(m))), ndigits = 0))
+
+            # Adjust for E12 and E24 quirks
+            if m == 12:
+                if n >=5 and n <= 8:
+                    r_high += int(0.1 * (10 ** decimals))
+                elif n == 11:
+                    r_high -= int(0.1 * (10 ** decimals))
+            elif m == 24:
+                if n >=10 and n <= 16:
+                    r_high += int(0.1 * (10 ** decimals))
+                elif n == 22:
+                    r_high -= int(0.1 * (10 ** decimals))
+            
+
+            if ((r_low <= r_value_adj) and
+                r_high >= r_value_adj):
+                if (r_value_adj - r_low) <= (r_high - r_value_adj):
+                    return r_low * (10 ** (exp - decimals))
+                else:
+                    return r_high * (10 ** (exp - decimals))
+            elif ((r_value_adj > r_high) and
+                   n == m - 1):
+                r_low = r_high
+                r_high = 1 * (10 ** (decimals + 1))
+                if (r_value_adj - r_low) <= (r_high - r_value_adj):
+                    return r_low * (10 ** (exp - decimals))
+                else:
+                    return r_high * (10 ** (exp - decimals))
+            else:
+                r_low = r_high
+        
+    def get_std_value(self) -> float:
+        
+        self.__r_std = self.__E_series(self.r_value, self.r_tolerance)
+        return self.__r_std
+
+
+if __name__ == "__main__":
+
+    resistor = Resistor()
+    
+    tolerances = [10, 5, 2, 1]
+    #tolerances = [10]
+
+    for t in tolerances:
+        for r in range(100, 1000):
+        
+            resistor.r_value = r
+            resistor.r_tolerance = t
+            r_std = resistor.get_std_value()
+
+            print(f"{resistor.r_value} - {resistor.r_tolerance} -> {r_std}")
+
---
+++ resistor_divider.py
@@ -0,0 +1,136 @@
+from resistor import Resistor
+
+import numpy as np
+
+class Resistor_Divider():
+    
+    @property
+    def input_voltage(self):
+        return self._input_voltage
+    
+    @input_voltage.setter
+    def input_voltage(self, value):
+        if value == 0:
+            raise ValueError(f"input_voltage must be != 0")
+
+        self._input_voltage = value
+    
+    @property
+    def output_voltage(self):
+        return self._output_voltage
+    
+    @output_voltage.setter
+    def output_voltage(self, value):
+        if value == 0:
+            raise ValueError(f"output_voltage must be != 0")
+
+        self._output_voltage = value
+
+    @property
+    def resistors_tolerance(self):
+        return self._resistors_tolerance
+    
+    @resistors_tolerance.setter
+    def resistors_tolerance(self, value):
+        if value <= 0 or value > 100:
+            raise ValueError(f"resistors_tolerance must be > 0 and <= 100")
+
+        self._resistors_tolerance = value
+    
+    @property
+    def divider_current(self):
+        return self._divider_current
+    
+    @divider_current.setter
+    def divider_current(self, value):
+        if value <= 0:
+            raise ValueError(f"divider_current must be > 0")
+
+        self._divider_current = value
+
+    __r_up = 0
+    __r_down = 0
+    __div_current = 0
+    __out_voltage = 0
+
+    def calc_divider(self) -> None:
+        
+        rs = self.input_voltage / self.divider_current
+        
+        actual_vout = 0
+        iterations = 0
+
+        while abs(self.output_voltage - actual_vout) > 0.01 and iterations < 100:
+            r_down = (self.output_voltage / self.input_voltage) * rs
+            #print(f"r_down: {r_down} = ({self.output_voltage} / {self.input_voltage}) * {rs}")
+            r_down_std = Resistor(value = r_down,
+                                tolerance = self.resistors_tolerance).get_std_value()
+            r_up = rs - r_down_std
+            r_up_std = Resistor(value = r_up,
+                                tolerance = self.resistors_tolerance).get_std_value()
+            rs = r_up_std + r_down_std
+            actual_vout = self.input_voltage * (r_down_std / rs)
+            #print(f"actual_vout: {actual_vout} = {self.input_voltage} * ({r_down_std} / {rs})")
+
+            iterations += 1
+            """ print(f"iter: {iterations}")
+            print(f"\tr_up_std: {r_up_std}")
+            print(f"\tr_down_std: {r_down_std}")
+            print(f"\tactual_vout: {actual_vout}") """
+        
+            self.__r_up = r_up_std
+            self.__r_down = r_down_std
+            self.__div_current = self.input_voltage / rs
+            self.__out_voltage = actual_vout
+    
+    def __get_divider(self) -> tuple[float, float]:
+
+        return (self.__r_up, self.__r_down)
+
+    def __get_div_current(self) -> float:
+
+        return self.__div_current
+    
+    def __get_output_voltage(self) -> float:
+
+        return self.__out_voltage
+
+    def print_divider_info(self) -> None:
+
+        print(f"Upper resistor: {self.__r_up}")
+        print(f"Lower resistor: {self.__r_down}")
+        print(f"Divider current: {self.__div_current}")
+        print(f"Output voltage: {self.__out_voltage}")
+    
+    def __init__(self,
+                 *,
+                 input_voltage: float,
+                 output_voltage: float,
+                 resistors_tolerance: int,
+                 divider_current: float) -> None:
+        
+        self.input_voltage = input_voltage
+        self.output_voltage = output_voltage
+        self.resistors_tolerance = resistors_tolerance
+        self.divider_current = divider_current
+    
+    def __str__(self) -> str:
+        
+        rd_properties = f"input_voltage: {self.input_voltage} V\n"\
+                        f"output_voltage: {self.output_voltage} V\n"\
+                        f"resistors_tolerance: {self.resistors_tolerance} %\n"\
+                        f"divider_current: {self.divider_current} A\n"
+        
+        return rd_properties
+
+if __name__ == "__main__":
+
+    resistor_divider = Resistor_Divider(input_voltage = 5.0,
+                                        output_voltage = 0.8,
+                                        resistors_tolerance = 1,
+                                        divider_current = 0.000085)
+    
+    print(resistor_divider)
+    
+    resistor_divider.calc_divider()
+    resistor_divider.print_divider_info()
